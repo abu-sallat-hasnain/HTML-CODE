4026329e6a5b92e6a4a4aaeeb216d980c6ea1ccf
Abu sallat raza <abusallat1214@gmail.com>
2025-11-12 20:26:28 +0500

    Add files via upload

---
+++ Class.cs
@@ -0,0 +1,13 @@
+﻿namespace MaktabeGulabProject.Models
+{
+    public class Class
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }       
+        public string Code { get; set; }
+        public int TotalStudents { get; set; }
+        public string Medium { get; set; }       
+        public string Description { get; set; }
+    }
+
+}
